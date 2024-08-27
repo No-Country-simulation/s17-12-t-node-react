2f2097fe30f12c80b404cca05ee71162b0f3b995
mingeniero <maximilianoingeniero@gmail.com>
2024-08-27 20:11:53 -0300

    feat: add user entity

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -6,11 +6,13 @@ import {
   Patch,
   Param,
   Delete,
+  NotFoundException,
+  HttpCode,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { User } from './user.schema';
+import { User } from './entities/user.entity';
 
 @Controller('user')
 export class UserController {
@@ -22,22 +24,25 @@ export class UserController {
   }
 
   @Get()
-  findAll() {
+  async findAll() {
     return this.userService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  async findOne(@Param('id') id: string) {
     return this.userService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  @HttpCode(204)
+  async remove(@Param('id') id: string): Promise<User> {
+    const task = await this.userService.remove(+id);
+    if (!task) throw new NotFoundException('Task not found');
+    return;
   }
 }
