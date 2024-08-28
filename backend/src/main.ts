62066192364549e77b55b714c67e3ee45b3c75f6
mingeniero <maximilianoingeniero@gmail.com>
2024-08-28 16:11:40 -0300

    feat: enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
