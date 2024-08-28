d467e2bc10f1596494bfa3535d1561b7ea91132e
isakidev <gaspar.c.developer@gmail.com>
2024-08-28 01:19:35 -0400

    feat: add mongoose and env config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AlbumModule } from './album/album.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL'),
+        user: configService.get<string>('MONGO_USERNAME'),
+        pass: configService.get<string>('MONGO_PASSWORD'),
+      }),
+    }),
+    AlbumModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
