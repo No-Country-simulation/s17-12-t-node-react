80dcb94ffc7493266440fff3e0096911c156001f
isakidev <gaspar.c.developer@gmail.com>
2024-08-28 01:21:25 -0400

    feat: add prefix and global validation pipes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api/v1');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const PORT = process.env.PORT;
+
+  await app.listen(PORT);
+
+  console.log(`http://localhost:${PORT}/api/v1`);
 }
 bootstrap();
