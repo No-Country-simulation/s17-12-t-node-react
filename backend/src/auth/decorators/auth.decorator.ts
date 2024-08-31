692ae9ca6df2e5cfac676b40619037d4f77c4010
isakidev <gaspar.c.developer@gmail.com>
2024-08-31 01:42:57 -0400

    feat: add user role verification

--- backend/src/auth/decorators/auth.decorator.ts
+++ backend/src/auth/decorators/auth.decorator.ts
@@ -1,8 +1,13 @@
 import { UseGuards, applyDecorators } from '@nestjs/common';
 
 import { AuthGuard } from '@nestjs/passport';
-import { RoleProtected } from './role-protected.decorator';
 
-export function Auth(isAdmin: boolean = false) {
-  return applyDecorators(RoleProtected(isAdmin), UseGuards(AuthGuard('jwt')));
+import { RoleProtected } from '.';
+import { UserRoleGuard } from '../guards/user-role.guard';
+
+export function Auth({ isAdmin }: { isAdmin: boolean } = { isAdmin: false }) {
+  return applyDecorators(
+    RoleProtected(isAdmin),
+    UseGuards(AuthGuard('jwt'), UserRoleGuard),
+  );
 }
