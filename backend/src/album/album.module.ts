a7da93681dc66b51881c7cae0970ab33f59676de
isakidev <gaspar.c.developer@gmail.com>
2024-09-06 14:42:26 -0400

    feat: import user module

--- backend/src/album/album.module.ts
+++ backend/src/album/album.module.ts
@@ -3,10 +3,12 @@ import { AlbumService } from './album.service';
 import { AlbumController } from './album.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Album, AlbumSchema } from './schemas/album.schema';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Album.name, schema: AlbumSchema }]),
+    UserModule,
   ],
   controllers: [AlbumController],
   providers: [AlbumService],
