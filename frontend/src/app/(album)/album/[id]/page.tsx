7af7c42b9bcb82ac1ae437ebde1ca4aeed47fd03
lazaronazareno <lazaronazareno@gmail.com>
2024-09-12 11:31:11 -0300

    fix(frontend): :bug: fix description not showing in production
    readOnlyEditor refactor, AlbumDetails and feed with default loaders

--- frontend/src/app/(album)/album/[id]/page.tsx
+++ frontend/src/app/(album)/album/[id]/page.tsx
@@ -1,28 +1,19 @@
-import { getAlbumsByTag } from "@/actions/albumActions";
-import { getUserById } from "@/actions/userActions";
 import { BackArrow } from "@/components";
-import AlbumDetails from "@/components/AlbumDetails";
-import { AlbumFromFetch } from "@/interfaces/album";
-import { User } from "@/interfaces/user";
-import Link from "next/link";
+import AlbumDetailsLoader from "@/components/AlbumDetailsLoader";
+import { Suspense } from "react";
 
-const BASE_URL = process.env.API_URL
 
-export default async function AlbumDetailsPage({ params }: { params: { id: string } }) {
+export default function AlbumDetailsPage({ params }: { params: { id: string } }) {
   const id = params.id
-  const url = BASE_URL + '/album/' + id
-  const data = await fetch(url)
-  const album: AlbumFromFetch = await data.json()
-
-  const relatedAlbums = await getAlbumsByTag(album.tags[0])
-  const user: User | null = await getUserById(album.userId)
 
   return (
     <div>
-      <div className="mx-4 mt-2 ">
+      <div className="mx-4 mt-2 flex">
         <BackArrow />
       </div>
-      <AlbumDetails album={album} relatedAlbums={relatedAlbums} user={user} />
+      <Suspense fallback={<span>Cargando Vista detallada</span>}>
+        <AlbumDetailsLoader id={id} />
+      </Suspense>
     </div>
   )
 }
